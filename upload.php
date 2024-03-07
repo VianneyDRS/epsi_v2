bcc86f706477a0eebda681a4e7d2fd4cad024829
Vianney de Raphelis Soissan <vianneyrs@gmail.com>
2024-03-07 09:22:03 +0100

    simplication CSS

--- upload.php
+++ upload.php
@@ -6,7 +6,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="css/upload.css">
+        <link rel="stylesheet" href="css/index.css">
         <title>Epsi v2</title>
     </head>
     <body>
