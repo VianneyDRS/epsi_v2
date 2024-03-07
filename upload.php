1f1575bcf4d23da3445c8ff46fa73cd29fde7dd7
Vianney de Raphelis Soissan <vianneyrs@gmail.com>
2024-03-07 11:33:16 +0100

    ajout de try catch

--- upload.php
+++ upload.php
@@ -33,32 +33,35 @@
         $dossier = 'upload/';
         $fichier = basename($_FILES['fichier']['name']);
 
-        if(empty($fichier)){ 
-            // Vérifie si un fichier a été sélectionné
-            echo 'Aucun fichier sélectionné';
-        } else {
-            // Récupère l'extension du fichier et définie celle autorisé
+        try {
+            if (empty($fichier)) {
+                throw new Exception('Aucun fichier sélectionné');
+            }
+    
+            // Récupère l'extension du fichier et définie celle autorisée
             $extension = strtolower(pathinfo($fichier, PATHINFO_EXTENSION));
             $extensionsAutorisees = array('jpg', 'jpeg', 'png', 'pdf');
-
+    
             // Vérifie si le fichier existe déjà
             $compteur_fichier = 1;
             $nouveauNom = $fichier;
-            while(file_exists($dossier . $nouveauNom)){
+            while (file_exists($dossier . $nouveauNom)) {
                 $nouveauNom = pathinfo($fichier, PATHINFO_FILENAME) . $compteur_fichier . '.' . $extension;
                 $compteur_fichier++;
             }
-
+    
             // Vérifie si l'extension du fichier est autorisée
-            if(in_array($extension, $extensionsAutorisees)){
-                if(move_uploaded_file($_FILES['fichier']['tmp_name'], $dossier . $nouveauNom)){
-                    echo 'Upload effectué avec succès !';
-                }else{
-                    echo 'Echec de l\'upload !';
-                }
-            }else{
-                echo 'Extension de fichier non autorisée !';
+            if (!in_array($extension, $extensionsAutorisees)) {
+                throw new Exception('Extension de fichier non autorisée !');
             }
+    
+            if (move_uploaded_file($_FILES['fichier']['tmp_name'], $dossier . $nouveauNom)) {
+                echo 'Upload effectué avec succès !';
+            } else {
+                throw new Exception('Echec de l\'upload !');
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
         }
     }
 ?>
